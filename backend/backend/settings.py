cf76a8273646ca9b3c188de047532e72d477ae13
alexgrate <alexgrate606@gmail.com>
2026-02-18 17:22:48 +0100

    Fix static files for admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,6 +131,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
